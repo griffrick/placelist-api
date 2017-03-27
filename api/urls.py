644474728b713da830a9fec2c2999c8dc9bdfc1d
kdenny <kdenny@deloitte.com>
2017-03-26 21:31:56 -0400

    Added User Registration views

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+
 from api import views
 
 urlpatterns = [
@@ -8,4 +10,9 @@ urlpatterns = [
 
     url(r'places/$', views.Place.as_view()),
     url(r'places/(?P<place_key>[0-9]+)/$', views.Place.as_view()),
+
+    url(r'register/$', views.Register.as_view()),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
+
 ]
--- placelist/settings.py
+++ placelist/settings.py
@@ -116,6 +116,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
